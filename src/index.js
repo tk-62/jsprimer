7c96b0b1a29f5ca384e48597204f8b195d52d2c4
tk <68478701+tk-62@users.noreply.github.com>
2021-09-13 11:50:03 +0900

    変数

--- src/index.js
+++ src/index.js
@@ -1,15 +1,2 @@
-/**
- * 読み始める前の事前準備
- */
-
-function hello(name) {
-  console.log("こんにちは" + name + "さん");
-}
-
-hello("TK");
-
-/**
- * JavaScriptとは
- */
-
-//  変数まで終了
+const bookTitle = "JavaScript Primer";
+console.log(bookTitle);
