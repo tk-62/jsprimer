3c31d5d15135a5456358a97cf06d154677a3f0ee
tk <68478701+tk-62@users.noreply.github.com>
2021-09-13 13:00:30 +0900

    let終了

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
-const bookTitle = "JavaScript Primer";
-console.log(bookTitle);
+// const bookTitle = "JavaScript Primer",
+//       bookCategory = "プログラミング";
+
+// // bookTitle = "JS"
+
+// console.log(bookTitle);
+// console.log(bookCategory);
+
+// let bookTitle = "JavaScript Primer";
+// let bookTitle;
+// bookTitle = "JavaScript Primer";
+// bookTitle = "HTML"
+// console.log(bookTitle);
+
+// var bookTitle;
+// bookTitle = "JavaScript Primer";
+// bookTitle = "HTML"
+// console.log(bookTitle);
+
+// let bookTitle = "JavaScript Primer";
+// bookTitle = "HTML";
+// console.log(bookTitle);
