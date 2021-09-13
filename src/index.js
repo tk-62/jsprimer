daf6950b7100917542da5f883d3e1ede44659413
tk <68478701+tk-62@users.noreply.github.com>
2021-09-13 15:43:05 +0900

    データ型とリテラルまで

--- src/index.js
+++ src/index.js
@@ -24,11 +24,48 @@
 // const TEN_NUMBER = 10;
 // console.log(TEN_NUMBER);
 
-const object = {
-  key: "10",
-  name: "TK"
-};
-object.key = "100";
+// const object = {
+//   key: "10",
+//   name: "TK"
+// };
+// object.key = "100";
 
-console.log(object.key);
-console.log(object.name);
+// console.log(object.key);
+// console.log(object.name);
+
+// const bookTitle = "Javascript Primer";
+// console.log(bookTitle);
+
+// console.log(1);
+// console.log(1 + 1);
+// const total = 42 + 42;
+// console.log(total);
+
+// console.log(typeof true);// => "boolean"
+// console.log(typeof 42); // => "number"
+// console.log(typeof 9007199254740992n); // => "bigint"
+// console.log(typeof "JavaScript"); // => "string"
+// console.log(typeof Symbol("シンボル"));// => "symbol"
+// console.log(typeof undefined); // => "undefined"
+// console.log(typeof null); // => "object"
+// console.log(typeof ["配列"]); // => "object"
+// console.log(typeof { "key": "value" }); // => "object"
+// console.log(typeof function() {}); // => "function"
+
+// const str = "こんにちは";
+// console.log(str);
+
+// console.log(0b1111); // => 15
+// console.log(0b10000000000); // => 1024
+
+// console.log(2e9);
+
+// const obj = {
+//   "key" : "value"
+// };
+// // console.log(obj.key);
+// console.log(obj["key"]);
+
+// const array = ["index:0","index:1","index:2"];
+// console.log(array[0]);
+// console.log(array[2]);
