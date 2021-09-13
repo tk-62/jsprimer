9ccb062ae3fe149efcf8f7db4ebf9344bb32cd85
tk <68478701+tk-62@users.noreply.github.com>
2021-09-14 06:55:18 +0900

    テンプレート文字列

--- src/index.js
+++ src/index.js
@@ -70,7 +70,54 @@
 // console.log(array[0]);
 // console.log(array[2]);
 
-const a = 1,
-  b = 2,
-  c = a + b;
-console.log(c);
+/**
+ * データ型とリテラル
+ */
+// console.log(typeof true);
+// console.log(typeof 42);
+// console.log(typeof 9007199254740992n);
+// console.log(typeof "JavaScript");
+// console.log(typeof Symbol("シンボル"));
+// console.log(typeof undefined);
+// console.log(typeof null);
+// console.log(typeof ["配列"]);
+// console.log(typeof {"key": "value"});
+// console.log(typeof function(){});
+
+// 基本的にtypeof演算子は、プリミティブ型またはオブジェクトかを判別するものです。
+// オブジェクトの詳細な種類を判定できない
+
+// const str = "こんにちは";
+// console.log(str);
+
+//10進数
+// console.log(1);
+// console.log(10);
+// console.log(255);
+
+//2進数
+// console.log(0b1111);
+// console.log(0b10000000000);
+
+//8進数
+// console.log(0o644);
+// console.log(0o777);
+
+//16進数
+// console.log(0xFF);
+// console.log(0xff);
+// console.log(0x30a2);
+
+// console.log(Number.MAX_SAFE_INTEGER);
+// console.log(1n);
+// console.log(9007199254740992n);
+// console.log(1.2n);
+
+// console.log(1_000_000_000_000);
+
+// console.log('8 o\'clock');
+// console.log(`8
+// o'clock`);
+
+// const str = "文字列";
+// console.log(`これは${str}です`);
